8890f8e971d174dde764c678c26f33cbf76245ee
abysschroma <121738096+abysschroma@users.noreply.github.com>
2026-01-12 15:18:25 +0900

    upgrade CE

--- src/main/java/com/leafia/AddonBase.java
+++ src/main/java/com/leafia/AddonBase.java
@@ -36,7 +36,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.File;
 
 @Mod(modid = Tags.MODID, version = "Unknown", name = Tags.MODNAME, acceptedMinecraftVersions = "[1.12.2]",
-		dependencies = "required-after:hbm@[1.4.0.3,);required:mixinbooter")
+		dependencies = "required-after:hbm@[1.4.2.4,);required:mixinbooter")
 public class AddonBase {
 
 	public static final Logger LOGGER = LogManager.getLogger(Tags.MODID);
